b1296a47cbdb1c3248315e881a51fc2864ac0570
Austin Lasher <austinlasher@knights.ucf.edu>
2016-12-15 00:04:41 -0500

    Fixed OSX build

--- include/slab/foundation.h
+++ include/slab/foundation.h
@@ -1,8 +1,8 @@
 #ifndef FOUNDATION_H
 #define FOUNDATION_H
 
-#include <GL/glew.h>
-#include <GL/gl.h>
+//#include <GL/glew.h>
+//#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 namespace Slab {
@@ -12,11 +12,13 @@ namespace Slab {
         ~Foundation();
         //bool isWindowActive();
         void activateWindow();
+        inline bool isAlive() { return window_creation_worked; }
 
     private:
         GLFWwindow *glfw_window;
+        bool window_creation_worked;
         static void glfw_keypress(GLFWwindow *w, int key, int scancode, int action, int mode);
-        //bool window_active;
+        static void glfw_err_callback(int errnum, const char *desc);
     };
 }
 
